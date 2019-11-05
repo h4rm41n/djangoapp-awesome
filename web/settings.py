2e27ce982cd804d286b240ed3616d58aa83db51a
Ahmad Harmain <mail.ahmad.harmain@gmail.com>
2019-11-05 23:24:13 +0800

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -75,17 +75,17 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kursus_web',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'kursus_web',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1'
+#     }
 }
 
 
